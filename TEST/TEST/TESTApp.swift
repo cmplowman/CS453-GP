45c3af2cbf0e20d7db148ddba27243b955f51e1d
Chris Plowman <cmplowman@loyola.edu>
2023-10-31 12:37:32 -0400

    test repo

---
+++ TEST/TEST/TESTApp.swift
@@ -0,0 +1,17 @@
+//
+//  TESTApp.swift
+//  TEST
+//
+//  Created by Student on 10/31/23.
+//
+
+import SwiftUI
+
+@main
+struct TESTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
