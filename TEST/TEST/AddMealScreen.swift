45f3d4c18e8dd52a53d455356424b5b1b1afe945
christophkp <kochpaizchristoph@gmail.com>
2023-11-02 19:48:10 -0400

    upated AddMealScreen - still needs work

--- TEST/TEST/AddMealScreen.swift
+++ TEST/TEST/AddMealScreen.swift
@@ -8,20 +8,96 @@
 import SwiftUI
 
 struct AddMealScreen: View {
+    @State var pickerSelection = "Eggs"
+    @State var customMeal = ""
+//    let meal1 = Meal(name: "Eggs", id: 1)
+//    let meal2 = Meal(name: "Bacon", id: 1)
+//    let meal3 = Meal(name: "Bacon", id: 1)
+    var mealsExample = ["Custom Meal", "Bacon", "Turkey", "Oatmeal"]
+    @State var favoritedMeals: [String] = []
+    @State var addedMeal: [String] = []
+
+    
+    var selectedMeals: [String] = []
+
+    
     var body: some View {
-        VStack {
-            
-            Button{
-               print("Tapped")
-            }label: {
-                Label("Add Meal", systemImage: "fork.knife.circle.fill")
+        
+        NavigationView{
+            VStack {
+                
+                
+                Form{
+                    Picker(selection: $pickerSelection, label: Text("Choose Meal"), content: {
+                        ForEach(mealsExample, id: \.self) { meal in
+                            Text(meal)
+                        }})
+                    
+                    
+                    if pickerSelection == "Custom Meal"
+                    {
+                        TextField("Custom Meal", text: $customMeal)
+                    }
+                    
+                }
+                
+                
+                //testing purposes
+                ForEach(favoritedMeals, id: \.self) { meal in
+                    Text(meal)
+                }
+                
+                
+                
+                
+                Button{
+                    if customMeal != ""
+                    {
+                        favoritedMeals.append(customMeal)
+                    }
+                    else
+                    {
+                        favoritedMeals.append(pickerSelection)
+                    }
+          
+                    
+                }label: {
+                    Label("Favorite", systemImage: "star.fill")
+                        .frame(maxWidth: .infinity)
+                        .font(.system(size: 30))
+                }
+                .buttonStyle(.borderedProminent)
+                .tint(.blue)
+                .controlSize(.large)
+                
+                Button{
+                    if customMeal != ""
+                    {
+                        addedMeal.append(customMeal)
+                    }
+                    else
+                    {
+                        addedMeal.append(pickerSelection)
+                    }
+
+                }label: {
+                    Label("Add Meal", systemImage: "fork.knife.circle.fill")
+                        .frame(maxWidth: .infinity)
+                        .font(.system(size: 30))
+                }
+                .buttonStyle(.borderedProminent)
+                .tint(.blue)
+                .controlSize(.large)
+                
+                
+                
             }
-            .buttonStyle(.bordered)
-            .tint(.blue)
-            
+            .navigationTitle("Add Meal")
         }
     }
 }
+
+//Model
 
 
 struct AddMealScreen_Previews: PreviewProvider {
