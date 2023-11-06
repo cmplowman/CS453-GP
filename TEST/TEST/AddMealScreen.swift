4d18a4c8212f9c10900d17e2014878573fe3c6e5
christophkp <kochpaizchristoph@gmail.com>
2023-11-06 13:27:24 -0500

    added alerts, code needs cleaning

--- TEST/TEST/AddMealScreen.swift
+++ TEST/TEST/AddMealScreen.swift
@@ -8,14 +8,19 @@
 import SwiftUI
 
 struct AddMealScreen: View {
-    @State var pickerSelection = "Eggs"
+    @State var pickerSelection = ""
+    @State var pickerSelection2 = ""
+
     @State var customMeal = ""
 //    let meal1 = Meal(name: "Eggs", id: 1)
 //    let meal2 = Meal(name: "Bacon", id: 1)
 //    let meal3 = Meal(name: "Bacon", id: 1)
-    var mealsExample = ["Custom Meal", "Bacon", "Turkey", "Oatmeal"]
+    var mealsExample = ["Custom Meal", "Favorites","Bacon", "Turkey", "Oatmeal"]
     @State var favoritedMeals: [String] = []
     @State var addedMeal: [String] = []
+    @State private var showingAlert = false
+
+    
 
     
     var selectedMeals: [String] = []
@@ -29,35 +34,64 @@ struct AddMealScreen: View {
                 
                 Form{
                     Picker(selection: $pickerSelection, label: Text("Choose Meal"), content: {
+                        
                         ForEach(mealsExample, id: \.self) { meal in
                             Text(meal)
                         }})
-                    
                     
                     if pickerSelection == "Custom Meal"
                     {
                         TextField("Custom Meal", text: $customMeal)
                     }
                     
+                    else if pickerSelection == "Favorites"
+                    {
+                        if favoritedMeals.isEmpty
+                        {
+                            //display an alert
+                            
+                        }
+                        Picker(selection: $pickerSelection2, label: Text("Select From Favorites"), content: {
+                            
+                            ForEach(favoritedMeals, id: \.self) { meal in
+                                Text(meal)
+                            }})
+                    }
                 }
                 
-                
-                //testing purposes
                 ForEach(favoritedMeals, id: \.self) { meal in
+                                    Text(meal)
+                                }
+                ForEach(addedMeal, id: \.self) { meal in
                     Text(meal)
                 }
                 
                 
-                
-                
+                //favorite button
                 Button{
-                    if customMeal != ""
+                    if !customMeal.isEmpty
                     {
                         favoritedMeals.append(customMeal)
                     }
+                    else if pickerSelection == "Custom Meal" && customMeal.isEmpty
+                    {
+                       //Do nothing
+                    }
+                    else if pickerSelection == "Favorites"
+                    {
+                        //alert that says already exists in favorites
+                        showingAlert.toggle()
+
+                    }
                     else
                     {
-                        favoritedMeals.append(pickerSelection)
+                        if favoritedMeals.contains(pickerSelection)
+                        {
+                            showingAlert.toggle()
+                        }
+                        else{
+                            favoritedMeals.append(pickerSelection)
+                        }
                     }
           
                     
@@ -70,10 +104,27 @@ struct AddMealScreen: View {
                 .tint(.blue)
                 .controlSize(.large)
                 
+                .alert(isPresented: $showingAlert) {
+                    
+                    Alert(
+                        title: Text("Duplicate Favorite"),
+                        message: Text("You have already favorited this Meal"),
+                        dismissButton: .cancel())
+                }
+                
+                //add meal button
                 Button{
                     if customMeal != ""
                     {
                         addedMeal.append(customMeal)
+                    }
+                    else if pickerSelection == "Favorites"
+                    {
+                        addedMeal.append(pickerSelection2)
+                    }
+                    else if pickerSelection == "Custom Meal" && customMeal.isEmpty
+                    {
+                        //Do nothing
                     }
                     else
                     {
@@ -96,6 +147,7 @@ struct AddMealScreen: View {
         }
     }
 }
+
 
 //Model
 
